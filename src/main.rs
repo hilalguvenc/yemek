fba2a15223785f132f8e439a99eee47a70245ccb
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-12-22 00:09:18 +0300

    add: client folder is served as /static

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use dotenv::from_filename;
-use log::info;
+use mime::TEXT_HTML_UTF_8;
 
-use actix_files::NamedFile;
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_files::{NamedFile, Files};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn index(req: HttpRequest) -> impl Responder {
-  NamedFile::open("./client/home.html").unwrap().into_response(&req)
+  NamedFile::open("./client/home.html")
+    .unwrap()
+    .set_content_type(TEXT_HTML_UTF_8)
+    .into_response(&req)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   from_filename(".env").ok();
+  env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
   let addr = format!("0.0.0.0:{}", std::env::var("PORT").unwrap());
-  let server = HttpServer::new(|| App::new().service(web::resource("/").to(index)))
-    .bind(&addr)
-    .unwrap();
-
-  info!("Starting http server: {}", &addr);
-  server.run().await
+  HttpServer::new(|| {
+    App::new()
+      .service(web::resource("/").to(index))
+      .service(Files::new("/static", "./client").prefer_utf8(true))
+  })
+  .bind(&addr)
+  .unwrap()
+  .run()
+  .await
 }
