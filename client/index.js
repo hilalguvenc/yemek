94691e7ca9ec2b095956a90accb104b71bf87943
Hilal <hilal.guvenc@segmentify.com>
2021-01-13 22:24:19 +0300

    swipe right

---
+++ client/index.js
@@ -0,0 +1,8 @@
+function swipeRight(){
+    document.getElementById(".card-container").addEventListener("click",function(){
+        document.getElementById(".card-container").classList.add("rotate-left");//fade ve delay verilecek
+    })
+    if(document.querySelectorAll(".card-container")){
+
+    }
+}
