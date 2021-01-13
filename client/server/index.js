04016db53dcc93422cb5b23d06865fc530ab0332
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2021-01-13 22:53:28 +0300

    add: server for client

---
+++ client/server/index.js
@@ -0,0 +1,27 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, '..'),
+  prefix: '/static/',
+});
+
+fastify.get('/', async (request, reply) => {
+  return reply.sendFile('index.html', path.join(__dirname, '../pages'));
+});
+
+fastify.get('/recipe', async (request, reply) => {
+  return reply.sendFile('recipe.html', path.join(__dirname, '../pages'));
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3003)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
