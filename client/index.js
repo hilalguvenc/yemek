a0c031be8b1c3d5fb0ab5bc99d11c58bcedd924f
Hilal <hilal.guvenc@segmentify.com>
2021-01-16 14:30:37 +0300

    updated

--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
 function swipeRight(){
-    document.getElementById(".card-container").addEventListener("click",function(){
-        document.getElementById(".card-container").classList.add("rotate-left");//fade ve delay verilecek
-    })
-    if(document.querySelectorAll(".card-container")){
-
+    document.getElementById(".card-container").classList.add("rotate-left");//fade ve delay verilecek css yazarken 
+    if(document.querySelectorAll(".card-container").lastChild){
+        document.getElementById('.card-container').childNodes[length-1].removeClass ('rotate-left rotate-right');
+    } else {
+        document.querySelectorAll(".card-container").nextElementSibling.remove('rotate-left rotate-right');
     }
 }
+
+const activeCard = document.querySelectorAll(".card-container.active")[0];
+activeCard.addEventListener("mousedown",swipeRight);
